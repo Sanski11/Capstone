7d76409aecbe6d5bd6c82ac4fe4fb0e11e49e139
Sanski11 <shannahhazelpalomo638@gmail.com>
2025-05-25 19:27:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'your_secret_key'
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Kitty_909'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'staff_portal'
 
 mysql = MySQL(app)
